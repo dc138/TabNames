c09ee0c49c64d468f78c49b74c97c427048428a8
DarthChungo <antonio@adaro.eu>
2023-06-26 22:39:35 +0200

    Initial commit

---
+++ src/main/java/me/DarthChungo/TabNames/mixins/ServerPlayerEntityMixin.java
@@ -0,0 +1,21 @@
+package me.DarthChungo.TabNames.mixins;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerPlayerEntity.class)
+public class ServerPlayerEntityMixin {
+  @Inject(method = "getPlayerListName", at = @At("HEAD"), cancellable = true)
+  private void styledPlayerList$changePlayerListName(CallbackInfoReturnable<Text> cir) {
+    try {
+      cir.setReturnValue(Text.of("test"));
+//        if (ConfigManager.isEnabled() && ConfigManager.getConfig().configData.playerName.changePlayerName) {
+//            cir.setReturnValue(ConfigManager.getConfig().formatPlayerUsername((ServerPlayerEntity) (Object) this));
+//        }
+    } catch (Exception e) {}
+  }
+}
